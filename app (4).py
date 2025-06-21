57aeb0ca0f522ce8e06637142794d20607bb4429
BhavaAkshaya5 <bhavaakshaya2006@gmail.com>
2025-06-21 11:13:05 +0530

    Update app (4).py

--- app (4).py
+++ app (4).py
@@ -7,10 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1SKj3PLjvvY4dRSU0kJVtNt07e5fuzeDw
 """
 
-
-# Ensure streamlit is installed
-!pip install streamlit
-
 import streamlit as st
 import pandas as pd
 import joblib  # For saving and loading the model
